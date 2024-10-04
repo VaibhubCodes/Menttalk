000a150bcbc106c99e4a8739dd3403b56336fc50
VaibhubCodes <167017011+VaibhubCodes@users.noreply.github.com>
2024-10-04 08:47:11 +0530

    Delete articles directory

--- articles/admin.py
+++
@@ -1,9 +0,0 @@
-# articles/admin.py
-from django.contrib import admin
-from .models import Article
-
-class ArticleInline(admin.TabularInline):
-    model = Article
-    extra = 1
-
-admin.site.register(Article)
