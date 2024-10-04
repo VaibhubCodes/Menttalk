05fbc6fcee6fc2dcf605823495d0a2b0ea2a53db
VaibhubCodes <167017011+VaibhubCodes@users.noreply.github.com>
2024-10-04 08:47:35 +0530

    Delete blogs directory

--- blogs/urls.py
+++
@@ -1,8 +0,0 @@
-# blogs/urls.py
-from django.urls import path
-from .views import BlogPostListCreateView, BlogPostDetailView
-
-urlpatterns = [
-    path('blogs/', BlogPostListCreateView.as_view(), name='blog-list-create'),
-    path('blogs/<int:pk>/', BlogPostDetailView.as_view(), name='blog-detail'),
-]
