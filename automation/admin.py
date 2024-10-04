d84fa1cf89ef899ab3495fdd8eecb2a9321f9b82
VaibhubCodes <167017011+VaibhubCodes@users.noreply.github.com>
2024-10-04 08:47:27 +0530

    Delete automation directory

--- automation/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import GoogleCredentials
-
-@admin.register(GoogleCredentials)
-class GoogleCredentialsAdmin(admin.ModelAdmin):
-    list_display = ['user']
--- automation/urls.py
+++
@@ -1,8 +0,0 @@
-# automation/urls.py
-from django.urls import path
-from .views import google_auth, oauth2callback
-
-urlpatterns = [
-    path('google-auth/', google_auth, name='google_auth'),
-    path('oauth2callback/', oauth2callback, name='oauth2callback'),
-]
