0cf95dc779ffc25d905dd947f7be6540fd963429
VaibhubCodes <167017011+VaibhubCodes@users.noreply.github.com>
2024-10-04 08:47:45 +0530

    Delete booking directory

--- booking/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Booking
-
-@admin.register(Booking)
-class BookingAdmin(admin.ModelAdmin):
-    list_display = ('mentor', 'mentee', 'start_time', 'end_time', 'payment_status', 'created_at')
-    list_filter = ('mentor', 'payment_status')
-    search_fields = ('mentor__username', 'mentee__username')
